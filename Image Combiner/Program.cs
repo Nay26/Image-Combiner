11c77a22cb8ba639081b46f668875d8893aaa22f
Naomi <naomiw@amdigital.co.uk>
2018-10-15 11:24:38 +0100

    It works now

--- Image Combiner/Program.cs
+++ Image Combiner/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,41 @@ namespace Image_Combiner
     {
         static void Main(string[] args)
         {
+            Image firstimage = Image.FromFile(@"C:\Users\Naomi\source\repos\Image Combiner\Image Combiner\bin\Debug\Creation\Hair\hair.png");
+            Image secondimage = Image.FromFile(@"C:\Users\Naomi\source\repos\Image Combiner\Image Combiner\bin\Debug\Creation\Head\head.png");
+            Bitmap output = MergeTwoImages(firstimage,secondimage);
+            output.Save(@"C:\Users\Naomi\source\repos\Image Combiner\Image Combiner\bin\Debug\Creation\CreatedImage\img.png", ImageFormat.Png);
         }
+
+        public static Bitmap MergeTwoImages(Image firstImage, Image secondImage)
+        {
+            if (firstImage == null)
+            {
+                throw new ArgumentNullException("firstImage");
+            }
+
+            if (secondImage == null)
+            {
+                throw new ArgumentNullException("secondImage");
+            }
+
+            int outputImageWidth = firstImage.Width;
+
+            int outputImageHeight = firstImage.Height;
+
+            Bitmap outputImage = new Bitmap(outputImageWidth, outputImageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(outputImage))
+            {
+                graphics.DrawImage(firstImage, new Rectangle(new Point(), firstImage.Size),
+                    new Rectangle(new Point(), firstImage.Size), GraphicsUnit.Pixel);
+                graphics.DrawImage(secondImage, new Rectangle(new Point(), secondImage.Size),
+                    new Rectangle(new Point(), secondImage.Size), GraphicsUnit.Pixel);
+            }
+
+            return outputImage;
+        }
+
+
     }
 }
